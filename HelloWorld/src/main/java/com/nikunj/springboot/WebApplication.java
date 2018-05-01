c18cd64a1ee2152826d003dff2d07b94a8baba06
Nikunj Patel <nikunjpatel1989@yahoo.com>
2018-05-01 23:53:20 +0530

    Update WebApplication.java

--- HelloWorld/src/main/java/com/nikunj/springboot/WebApplication.java
+++ HelloWorld/src/main/java/com/nikunj/springboot/WebApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-//PATEL
+//NEW CHANGE
 @SpringBootApplication
 public class WebApplication extends SpringBootServletInitializer {
 	@Override
