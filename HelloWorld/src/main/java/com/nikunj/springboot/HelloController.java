eb7280723c430e5c8a37cfffd0412b4b7629c7e1
Nikunj Patel <nikunjpatel1989@yahoo.com>
2018-05-02 23:32:28 +0530

    Update HelloController.java

--- HelloWorld/src/main/java/com/nikunj/springboot/HelloController.java
+++ HelloWorld/src/main/java/com/nikunj/springboot/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 //RARRRRR
 //ABCD
+//INITIAL
 @Controller
 public class HelloController {
     @RequestMapping("/")
