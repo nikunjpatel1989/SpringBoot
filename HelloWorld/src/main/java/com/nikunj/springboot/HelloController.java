87c8c4cb8ae6a5ee0d2b6bdfed6f62ac512027ad
Nikunj Patel <nikunjpatel1989@yahoo.com>
2018-05-02 21:41:20 +0530

    inital c

--- HelloWorld/src/main/java/com/nikunj/springboot/HelloController.java
+++ HelloWorld/src/main/java/com/nikunj/springboot/HelloController.java
@@ -5,8 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-//PATELABCD
-
 @Controller
 public class HelloController {
     @RequestMapping("/")
